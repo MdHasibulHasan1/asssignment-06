c1f57943f89ed063f67e6a69d376bb412e2bb99c
hasib1511 <hasib7143@gmail.com>
2023-03-02 21:45:05 +0600

    added modal

--- js/index.js
+++ js/index.js
@@ -7,11 +7,11 @@ const loadToolsData = async() =>{
 
 const displayToolsDetails=(data)=>{
     // console.log(data.tools);
-    const toolsContainer = document.getElementById('tools-container');
+    const toolsCard = document.getElementById('tools-card-container');
     data.tools.forEach(singleTool => {
         const {image, features, name, id}=singleTool;
-        console.log(singleTool);
-        toolsContainer.innerHTML+=`
+        // console.log(singleTool);
+        toolsCard.innerHTML+=`
         <div class="col">
             <div class="card">
             <img src="${image}" class="card-img-top" alt="...">
@@ -44,8 +44,31 @@ const loadToolDetails=async(id)=>{
     const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data);
+    displayDetailsInModel(data.data);
 }
-const displayDetailsInModel=(data)=>{
-
+const displayDetailsInModel=(singledetails)=>{
+    console.log(singledetails);
+    
+    const {description, image_link}=singledetails;
+    
+    const toolDetails=document.getElementById('tool-details');
+    toolDetails.innerHTML = `
+    <div class="col">
+        <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">${description}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card">
+        <img src="${image_link[0]}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        </div>
+    </div>
+    `
 }
