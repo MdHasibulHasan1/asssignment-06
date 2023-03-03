794d7a1668de73dfc39a34619a290834d0c46d9d
hasib1511 <hasib7143@gmail.com>
2023-03-03 20:58:25 +0600

    modal data validation

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ const seeMoreCard=async()=>{
 }
 // display Tools Details 
 const displayToolsDetails=(tools)=>{
-    console.log(tools[2].published_in);
+    // console.log(tools[2].published_in);
     // display 6 cards
     const seeMoreSection = document.getElementById('see-more-section');
     if(tools.length > 6) {
@@ -42,19 +42,19 @@ const displayToolsDetails=(tools)=>{
     
     const toolsCard = document.getElementById('tools-card-container');
     tools.forEach(singleTool => {
-        console.log(singleTool);
+        // console.log(singleTool);
         const {image, published_in, features, name, id}=singleTool;
-        console.log(singleTool);
+        // console.log(features);
         toolsCard.innerHTML+=`
         <div class="col">
             <div class="card">
-            <img src="${image}" class="card-img-top" alt="...">
+            <img src="${image ? image : "Not found"}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Features</h5>
                 <ol type="1">
-                    <li>${features[0]}</li>
-                    <li>${features[1]}</li>
-                    <li>${features[2]}</li>
+                    <li>${features[0] ? features[0] : 'Not available'}</li>
+                    <li>${features[1] ? features[1] : 'Not available'}</li>
+                    <li>${features[2] ? features[2] : 'Not available'}</li>
                 </ol>
                 <hr>
                 <div class="d-flex justify-content-between align-items-center">
@@ -66,7 +66,7 @@ const displayToolsDetails=(tools)=>{
                         </div>
                     </div>
                     <div>
-                        <i onclick="loadToolDetails('${id}')" class="fa-solid fa-arrow-right" data-bs-toggle="modal" data-bs-target="#toolDetailsModal"></i>
+                        <i onclick="loadToolDetails('${id}')" class="fa-solid rounded-circle p-2 bg-danger-subtle fa-arrow-right" data-bs-toggle="modal" data-bs-target="#toolDetailsModal"></i>
                     </div>
                 </div>
             </div>
@@ -82,44 +82,46 @@ const loadToolDetails=async(id)=>{
     const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
     const res = await fetch(url);
     const data = await res.json();
-    displayDetailsInModel(data.data);
+    displayDetailsInModel(data?.data);
 }
 // Display data in the modal
 const displayDetailsInModel=(singleDetails)=>{
-    console.log(singleDetails.pricing);
+    // console.log(singleDetails.image_link);
     
-    const {description, image_link, pricing, input_output_examples, features, integrations, accuracy}=singleDetails;
+    const {description, input_output_examples, image_link, pricing, features, integrations, accuracy}=singleDetails;
+   console.log(pricing? pricing[0].price : "Free of Cost/");
     let allFeatures= [];
     for (const key in features) {
         if (features.hasOwnProperty.call(features, key)) {
             const feature = singleDetails.features[key];
             console.log(feature.feature_name);
-            allFeatures.push(feature.feature_name);
-            console.log(accuracy.score);
+            allFeatures.push(feature?.feature_name);
+            console.log(singleDetails?.pricing);
             const toolDetails=document.getElementById('tool-details');
     toolDetails.innerHTML='';
     toolDetails.innerHTML += `
+
     <div class="col">
-        <div class="card bg-danger bg-danger-subtle border border-warning rounded">
+        <div class="card bg-danger bg-danger-subtle border border-warning rounded ">
+           
             <div class="card-body">
-                <h5 class="card-title">${description}</h5>
+            <h5 class="card-title">${description}</h5>
                 <div class="row row-cols-3 d-flex justify-content-between text-center">
-                    <div style="width: 30%" class="text-success col bg-white rounded p-2">
-                        <div class="fs-6">${pricing[0].price ? pricing[0].price : 'Free of Cost/'}</div>
-                        <div class="fs-6">${pricing[0].plan ? pricing[0].plan : 'Basic'}</div>
+                    <div style="width: 32%" class="text-success col bg-white rounded ">
+                        <div class="fs-6">${pricing? pricing[0].price : "Free of Cost/"}</div>
+                        <div class="fs-6">${pricing? pricing[0].plan : 'Basic'}</div>
                     </div>
-                    <div style="width: 30%" class="text-warning col bg-white rounded p-2">
-                        <div class="fs-6">${pricing[1].price ? pricing[1].price :'Free of Cost/'}</div>
-                        <div class="fs-6">${pricing[1].plan ? pricing[1].plan : 'Pro'}</div>
+                    <div style="width: 32%" class="text-warning col bg-white rounded ">
+                        <div class="fs-6">${pricing? pricing[1].price : "Free of Cost/"}</div>
+                        <div class="fs-6">${pricing? pricing[1].plan : 'Pro'}</div>
                     </div>
-                    <div style="width: 30%" class="text-danger col bg-white rounded p-2">
-                        <div class="fs-6">${pricing[2].price ? pricing[2].price :'Free of Cost/'}</div>
-                        <div class="fs-6">${pricing[2].plan ? pricing[2].plan : 'Enterprise'}</div>
+                    <div style="width: 32%" class="text-danger col bg-white rounded ">
+                        <div class="fs-6">${pricing? pricing[2].price : "Free of Cost/"}</div>
+                        <div class="fs-6">${pricing? pricing[2].plan : 'Enterprise'}</div>
                     </div>
                 </div>
-
+                
                 <div class="d-flex gap-4 justify-content-between">
-
                     <div class="text-bg-black ">
                         <h5 class="card-title fw-bold">Features</h5>
                         <ul>
@@ -131,28 +133,31 @@ const displayDetailsInModel=(singleDetails)=>{
                     <div class="">
                         <h5 class="card-title fw-bold">Integrations</h5>
                         <ul>
-                            <li>${singleDetails.integrations[0]}</li>
-                            <li>${singleDetails.integrations[1]}</li>
-                            <li>${singleDetails.integrations[2]}</li>
+                            <li>${integrations? integrations[0] : "No data Found"}</li>
+                            <li>${integrations? integrations[1] : ""}</li>
+                            <li>${integrations? integrations[2] : ""}</li>
                         </ul>
                     </div>
                 </div>
-
+                
             </div>
         </div>
-    </div>
-    <div class="col">
+  </div>
+  <div class="col">
         <div class="card">
-        <div class="position-relative">
-            <span class="badge text-bg-danger position-absolute top-0 mt-2 me-2 end-0">${accuracy.score ? accuracy.score*100 +'% accuracy' :''}</span>
-            <img src="${image_link[0] ? image_link[0] : "image not available"}" class="card-img-top" alt="...">
+                <div class="position-relative">
+                    <span class="badge text-bg-danger position-absolute top-0 mt-2 me-2 end-0">${accuracy.score ? accuracy.score*100+' % accuracy' : ''}</span>
+                    <img src="${image_link[0] ? image_link[0] : "Not found"}" class="card-img-top" alt="...">
+                </div>
+            <div class="card-body">
+            <h5 class="card-title text-center">${input_output_examples? input_output_examples[0].input : "Can you give any example?"}</h5>
+            <p class="card-title text-center">${input_output_examples? input_output_examples[0].output : "No! Not Yet! Take a break!!!"}</p>
+            </div>
         </div>
-        <div class="card-body my-2">
-        <h5 class="card-title">${input_output_examples[0].input ? input_output_examples[0].input : "Can you give any example?"}</h5>
-        <p class="card-title">${input_output_examples[0].output ? input_output_examples[0].output : "No! Not Yet! Take a break!!!"}</p>
-        </div>
-        </div>
-    </div>
+  </div>
+  
+
+    
     `;
 
         }
